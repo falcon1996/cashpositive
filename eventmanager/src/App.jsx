d3c78c3b91c387339eebb0e338b94e7985b37047
Dhruv Parashar <dhruvparashar6@gmail.com>
2018-01-01 19:28:04 +0000

    add Individual Post API

--- eventmanager/src/App.jsx
+++ eventmanager/src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "isomorphic-fetch";
 
-
 class Events extends React.Component {
     
     constructor(props){
         super(props);
         this.state = {
             event : 'party',
-            all: []
+            all: [],
+            mycomments:[]
         }
     }
     
@@ -26,13 +26,53 @@ class Events extends React.Component {
                 });
                 
             })
-            
     }
+    
+    
+    /*
+    Add an input form for this
+    
+    specific = (organiser) => {
+        
+        fetch('http://eventmanager-server.herokuapp.com/events?organiser='+organiser)
+            .then( (response) => {return response.json(); })
+            .then( (data) => {
+                console.log(data);
+                const newitems= data.all.map((newitem,i) => {
+                    
+                })    
+            });
+        
+    }
+    */
+    
+    details = (id) => {
+        
+        fetch('http://eventmanager-server.herokuapp.com/comments')
+            .then( (response) => {return response.json(); })
+            .then( (data) => {
+                console.log(data);
+                console.log(id);
+                
+                const comments= data.map((comment,i) => {
+                    if(id == comment.postId){
+                        return <li key={i}>{comment.body} </li>
+                    }
+                }) 
+                
+                this.setState({
+                    mycomments: [comments]
+                })
+            });
+        
+    }
+    
+    
     
     render(){
         
         const items= this.state.all.map((item,i) => {
-            return <li key={i}>{item.title} {item.organiser}</li>
+            return <li key={i}>{item.title} {item.organiser} <button onClick={() => this.details(item.id)} > More </button> </li>
         })
         
         return(
@@ -40,6 +80,7 @@ class Events extends React.Component {
                 <h1>Working</h1>
                 <ul>
                     {items}
+                    {this.state.mycomments}
                 </ul>
             </div>
         );       
